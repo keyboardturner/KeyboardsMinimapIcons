85f20a17e0f7bfd8351c8c97474d6f34cee5c7bb
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2020-11-18 08:51:37 -0800

    fixed player/NPC, added silver gathering nodes

--- KeyboardsMinimapIcons.lua
+++ KeyboardsMinimapIcons.lua
@@ -5,3 +5,15 @@ end
 local frame=CreateFrame("Frame","KeyboardsMinimapIconsFrame");
 frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:SetScript("OnEvent",eventHandler);
+
+SLASH_KMI_ENABLE1 = "/kmienable"
+SlashCmdList["KMI_ENABLE"] = function(msg)
+	Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\KeyboardsMinimapIcons");
+	print("|caa2788e3KMI|r: Enabling Keyboard's Minimap Icons");
+end
+
+SLASH_KMI_DISABLE1 = "/kmidisable"
+SlashCmdList["KMI_DISABLE"] = function(msg)
+	Minimap:SetBlipTexture("Interface\\MINIMAP\\ObjectIconsAtlas");
+	print("|caa2788e3KMI|r: Temporarily Disabling Keyboard's Minimap Icons")
+end
