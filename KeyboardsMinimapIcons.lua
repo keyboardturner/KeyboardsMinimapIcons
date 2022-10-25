15d842f78bc4e274488a09e6cec5aac79e61a2f2
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2022-10-25 04:55:08 -0700

    updates for dragonflight
    Better chat commands have now been implemented. you can now choose between 3 different texture styles - default, circle, and blue.

--- KeyboardsMinimapIcons.lua
+++ KeyboardsMinimapIcons.lua
@@ -1,19 +1,106 @@
-local function eventHandler(self,event,...)
-	Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\KeyboardsMinimapIcons");
+local _dummy, kmiFrame = ...; -- handles slash commands
+
+local defaultsTable = {"Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\KeyboardsMinimapIcons",
+}
+
+local kmiFrame = CreateFrame("Frame", "KeyboardsMinimapIconsFrame");
+
+function kmiFrame.eventHandler(self,event,...)
+	Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\KeyboardsMinimapIcons");
+	if not KMI_DB then
+		KMI_DB = defaultsTable;
+	else
+		Minimap:SetBlipTexture(KMI_DB);
+	end
 end
 
-local frame=CreateFrame("Frame","KeyboardsMinimapIconsFrame");
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
-frame:SetScript("OnEvent",eventHandler);
+kmiFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
+kmiFrame:SetScript("OnEvent",kmiFrame.eventHandler);
 
-SLASH_KMI_ENABLE1 = "/kmienable"
-SlashCmdList["KMI_ENABLE"] = function(msg)
-	Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\KeyboardsMinimapIcons");
-	print("|caa2788e3KMI|r: Enabling Keyboard's Minimap Icons");
+kmiFrame.coloredText = "|caa2788e3KMI|r"
+kmiFrame.coloredTextVerbose = "|caa2788e3Keyboard's Minimap Icons|r"
+
+
+function kmiFrame:Print(...)
+	local prefix = string.format(kmiFrame.coloredText .. ":");
+	DEFAULT_CHAT_FRAME:AddMessage(string.join(" ", prefix, ...));
 end
 
-SLASH_KMI_DISABLE1 = "/kmidisable"
-SlashCmdList["KMI_DISABLE"] = function(msg)
-	Minimap:SetBlipTexture("Interface\\MINIMAP\\ObjectIconsAtlas");
-	print("|caa2788e3KMI|r: Temporarily Disabling Keyboard's Minimap Icons")
+kmiFrame.commands = {
+	["enable"] = function()
+		Minimap:SetBlipTexture(KMI_DB[1]);
+		kmiFrame:Print("Enabling " .. kmiFrame.coloredTextVerbose .. ".");
+	end,
+
+	["disable"] = function()
+		Minimap:SetBlipTexture("Interface\\MINIMAP\\ObjectIconsAtlas");
+		kmiFrame:Print("Temporarily Disabling " .. kmiFrame.coloredTextVerbose .. ".")
+	end,
+
+	["default"] = function()
+		Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\KeyboardsMinimapIcons");
+		KMI_DB = "Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\KeyboardsMinimapIcons";
+		kmiFrame:Print("Enabling " .. "|caaf5e042" .. "default" .. "|r" .. " option.");
+	end,
+
+	["circle"] = function()
+		Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\NodesAsCircles");
+		KMI_DB = "Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\NodesAsCircles";
+		kmiFrame:Print("Enabling " .. "|caaf5e042" .. "circle" .. "|r" .. " option.");
+	end,
+
+	["blue"] = function()
+		Minimap:SetBlipTexture("Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\BlueNodes");
+		KMI_DB = "Interface\\AddOns\\KeyboardsMinimapIcons\\tex\\BlueNodes";
+		kmiFrame:Print("Enabling " .. "|caaf5e042" .. "blue" .. "|r" .. " option.");
+	end,
+
+	["help"] = function()
+		kmiFrame:Print("Thank you for using " .. kmiFrame.coloredTextVerbose .. ". This addon replaces the default minimap icons, particularly for Hunter Tracking as well as coloring gathering nodes blue.\nCommands available: " .. "|caaf5e042" .. "/kmi disable" .. "|r" .. " | " .. "|caaf5e042" .. "/kmi enable" .. "|r" .. " | " .. "|caaf5e042" .. "/kmi default" .. "|r" .. " | " .. "|caaf5e042" .. "/kmi circle" .. "|r" .. " | " .. "|caaf5e042" .. "/kmi blue" .. "|r" .. ".")
+	end
+};
+
+local function HandleSlashCommands(str)
+	if (#str == 0) then
+		kmiFrame.commands.help();
+		return;
+		end
+
+		local args = {};
+		for _dummy, arg in ipairs({ string.split(' ', str) }) do
+		if (#arg > 0) then
+			table.insert(args, arg);
+			end
+			end
+
+			local path = kmiFrame.commands; -- required for updating found table.
+
+			for id, arg in ipairs(args) do
+
+			if (#arg > 0) then --if string length is greater than 0
+			arg = arg:lower();          
+			if (path[arg]) then
+				if (type(path[arg]) == "function") then
+					-- all remaining args passed to our function!
+					path[arg](select(id + 1, unpack(args))); 
+					return;                 
+				elseif (type(path[arg]) == "table") then
+					path = path[arg]; -- another sub-table found!
+				end
+				else
+					kmiFrame.commands.help();
+				return;
+			end
+		end
+	end
 end
+
+function kmiFrame:init(event, name)
+	if (name ~= "KeyboardsMinimapIcons") then return end
+	SLASH_KMI1 = "/kmi"
+	SlashCmdList.KMI = HandleSlashCommands;
+end
+
+local events = CreateFrame("Frame");
+events:RegisterEvent("ADDON_LOADED");
+events:SetScript("OnEvent", kmiFrame.init);
